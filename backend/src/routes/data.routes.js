4241df70db0a318ae9e027f8bc7996c389ec18fe
ziSirrush <zspcktra@gmail.com>
2026-06-16 20:19:51 -0600

    Corrige conflictos de merge

--- backend/src/routes/data.routes.js
+++ backend/src/routes/data.routes.js
@@ -4,10 +4,7 @@ const router = express.Router();
 const dataController = require('../controllers/data.controller');
 
 router.get('/tickets', dataController.getTickets);
-<<<<<<< HEAD
 router.post('/tickets/sync', dataController.syncTickets);
-=======
->>>>>>> b99d62a5f077ac3c5abe3234a47b0f80370adc53
 
 router.get('/portafolio', dataController.getPortafolio);
 router.get('/equipos', dataController.getEquipos);
@@ -21,10 +18,4 @@ router.get('/roles', dataController.getRoles);
 router.get('/zonas', dataController.getZonas);
 router.get('/usuario-zop', dataController.getUsuarioZop);
 
-<<<<<<< HEAD
 module.exports = router;
-=======
-router.post('/tickets/sync',dataController.sybcTickets);
-
-module.exports = router;
->>>>>>> b99d62a5f077ac3c5abe3234a47b0f80370adc53
