38b8f6ef03799c81e504e70579766715e0d7051b
ziSirrush <zspcktra@gmail.com>
2026-07-09 16:13:37 -0600

    Reorganiza frontend a la raíz y prepara GitHub Pages

---
+++ core/config.js
@@ -0,0 +1,3 @@
+(function(){
+  window.MANTTO_API_BASE = window.MANTTO_API_BASE || 'https://mantto-gestor-api-a4hwfpgvbeb4gqmj.mexicocentral-01.azurewebsites.net';
+})();
--- pruebas/core/config.js
+++
@@ -1,3 +0,0 @@
-(function(){
-  window.MANTTO_API_BASE = window.MANTTO_API_BASE || 'https://gestormantto-production.up.railway.app';
-})();
