4a98a7067e11ef27514a1c4bd04875e0ca84787f
ziSirrush <zspcktra@gmail.com>
2026-08-06 20:19:49 -0600

    Experimental .5

--- backend/src/modules/experimental-entregas-recientes/experimental-entregas-recientes.repository.js
+++ backend/src/modules/experimental-entregas-recientes/experimental-entregas-recientes.repository.js
@@ -6,4 +6,9 @@ async function query_uni(sql, params) {
   return db.query(sql, params);
 }
 
-module.exports = { query_uni };
+module.exports = { query_uni, query };
+
+
+async function query(sql, params){
+  return query_uni(sql, params);
+}
