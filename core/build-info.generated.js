e1616ee50ecd4fe20486e588627560692b45fc59
ziSirrush <zspcktra@gmail.com>
2026-09-08 13:53:53 -0600

    Version 090826.1 - Comments

--- core/build-info.generated.js
+++ core/build-info.generated.js
@@ -2,8 +2,8 @@ window.MANTTO_BUILD_INFO = Object.freeze({
   "environment": "DEPLOY",
   "provider": "NETLIFY",
   "localVersion": "FIX V016.2",
-  "message": "Version 083026.11 - Organizacion",
-  "commit": "fac20ae02c06a33022c02e673a11d61d04f4ed49",
-  "commitShort": "fac20ae",
-  "generatedAt": "2026-08-31T05:41:57.298Z"
+  "message": "Version 090726.2 - Comments",
+  "commit": "423d2afea4364120d7c665397caf4a172db68e33",
+  "commitShort": "423d2af",
+  "generatedAt": "2026-09-07T23:11:18.776Z"
 });
